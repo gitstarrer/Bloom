f00825bc6e6619af3cdfbbd822b312b7ee18b7d6
Himanshu Gupta <h23himanshu@gmail.com>
2025-08-18 19:28:58 +0530

    Initial Commit

---
+++ Bloom/BloomApp.swift
@@ -0,0 +1,17 @@
+//
+//  BloomApp.swift
+//  Bloom
+//
+//  Created by Himanshu on 18/08/25.
+//
+
+import SwiftUI
+
+@main
+struct BloomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
